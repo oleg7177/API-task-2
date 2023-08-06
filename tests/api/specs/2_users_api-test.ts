c9114af92e32458eac25fa4e6b30e01e996a974b
Oleg Kuziv <kuzivoleg7177@gmail.com>
2023-08-06 11:45:29 +0300

    additional tests added

--- tests/api/specs/2_users_api-test.ts
+++ tests/api/specs/2_users_api-test.ts
@@ -9,8 +9,15 @@ const chai = require('chai');
 chai.use(require('chai-json-schema'));
 
 describe(`Users controller`, () => {
-    let userId: '1747';
-    let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6IkpXVCJ9.eyJzdWIiOiJvbGVnNzE3NyIsImVtYWlsIjoia3V6aXZvbGVnNzE3N0BnbWFpbC5jb20iLCJqdGkiOiJkMDkxMjc0MS1lMzY3LTRiZTEtYmYyMi1jOGIxMjYyZjU5NWYiLCJpYXQiOjE2OTEzMDI3ODksImlkIjoiMTc0NCIsIm5iZiI6MTY5MTMwMjc4OSwiZXhwIjoxNjkxMzA5OTg5LCJpc3MiOiJUaHJlYWQuTkVUIFdlYkFQSSIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzQ0In0.2cORC3Lf6M0ok2ZKC_yid2ER7gsR8_7xeQdhGZQxzes"
+
+    let accessToken : string;
+
+    before(`Login and get the token`, async () => {
+        let response = await auth.login("kuzivoleg7177@gmail.com", "password1111");
+
+        accessToken = response.body.token.accessToken.token;
+        // console.log(accessToken);
+    });
 
     it(`New user account registered`, async () => {
         let response = await auth.registration(0,"myavatar","kuzivoleg7177@gmail.com","oleg7177","password1111");
@@ -19,7 +26,8 @@ describe(`Users controller`, () => {
         // console.log(response.body);
 
         expect(response.statusCode, `Status Code should be 201`).to.be.equal(201);
-        expect(response.timings.phases.total, `Response time should be less than 1s`).to.be.lessThan(1000); 
+        expect(response.timings.phases.total, `Response time should be less than 1s`).to.be.lessThan(1000);
+        expect(response.body.user.email, `User email should be equal to registered`).to.be.equal('kuzivoleg7177@gmail.com'); 
     });
 
     it(`Get all users`, async () => {
@@ -27,6 +35,7 @@ describe(`Users controller`, () => {
 
         expect(response.statusCode, `Status Code should be 200`).to.be.equal(200);
         expect(response.timings.phases.total, `Response time should be less than 1s`).to.be.lessThan(1000); 
+        
 
     });
 
@@ -34,7 +43,8 @@ describe(`Users controller`, () => {
        let response = await auth.login("kuzivoleg7177@gmail.com","password1111");
 
         expect(response.statusCode, `Status Code should be 200`).to.be.equal(200);
-        expect(response.timings.phases.total, `Response time should be less than 1s`).to.be.lessThan(1000); 
+        expect(response.timings.phases.total, `Response time should be less than 1s`).to.be.lessThan(1000);
+        expect(response.body.token.accessToken.expiresIn, `The token should expire in 7200`).to.be.equal(7200) 
        
     });
 
@@ -61,17 +71,20 @@ describe(`Users controller`, () => {
     });
 
     it(`Delete user by ID`, async () => {
-        let response = await users.deleteUserByID(accessToken,"1783")
+        let response = await users.deleteUserByID(accessToken,"1748")
 
         expect(response.statusCode, `Status Code should be 204`).to.be.equal(204);
         expect(response.timings.phases.total, `Response time should be less than 1s`).to.be.lessThan(1000); 
+        expect(response.body.id, `User id should be equal`).to.be.equal(1748);
+
     });
 
     it(`Comment a post`, async () => {
-        let response = await users.commentPost(accessToken,28,41,"stringstringstring")
+        let response = await users.commentPost(accessToken,1744,41,"stringstringstring")
 
         expect(response.statusCode, `Status Code should be 204`).to.be.equal(200);
         expect(response.timings.phases.total, `Response time should be less than 1s`).to.be.lessThan(1000); 
+        expect(response.body.author.id, `User id should be equal`).to.be.equal(1744);
     });
 
     it(`Get all posts`, async () => {
@@ -113,6 +126,12 @@ describe(`Users controller`, () => {
         expect(response.timings.phases.total, `Response time should be less than 1s`).to.be.lessThan(1000);  
     });
  
+    it(`Should return 404 error when updating user info with wrong id`, async () => {
+        let response = await users.updateUserInfo(17451745,"myavatar","kuzivoleg7177@@@gmail.com","oleg7177@@@",accessToken)
+
+        expect(response.statusCode, `Status Code should be 404`).to.be.equal(404);
+        expect(response.timings.phases.total, `Response time should be less than 1s`).to.be.lessThan(1000); 
+    });
 
 
    
